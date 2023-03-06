2bff02a8f45d2b354bf4543847811bf1996a68e0
Thias <Thias@DESKTOP-DH0CAPG>
2023-03-06 14:18:33 +0100

    Add project files.

---
+++ opgave-06-03-2023/Program.cs
@@ -0,0 +1,86 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using System.Globalization;
+
+// Thias Schou Dehlendorff Opgave Svar
+
+
+using (var reader = new StreamReader("Prisliste.csv"))
+{
+    List<Vare> VareList = new List<Vare>();
+    int i = 0;
+    Console.WriteLine(" Kurs Euro to dkk = 7.44");
+    // Her checker jeg om det er den første entry eller ej hvis ja springer den over for ellers for jeg denne linje med "Item;article description;unit;kostpris EUR;price unit;price group;date of issuance Vil jeg ikke :D
+    while (reader.EndOfStream == false)
+    {
+        if(i == 0)
+        {
+
+            var line = reader.ReadLine();
+        }
+        else
+        {
+            var line = reader.ReadLine();
+            //her spiltter jeg linjen op ved alle ";" Så jeg har det forskellige elementer
+            List<string> strVare = line.Split(';').ToList();
+            Console.WriteLine(i);
+            // Her instancere jeg min nye fine vare og parser dem så jeg kan få dem i Float og Int variabler og Bruger min ItemUpdater og min priceGroupUpdater
+            VareList.Add(new Vare(strVare[0], itemUpdater(strVare[1]), strVare[2], float.Parse(strVare[3].Substring(0, strVare[3].Length - 2)), float.Parse(strVare[4]), priceGroupUpdate(strVare[5]), DateTime.Parse(strVare[6])));
+        }
+       i++;
+        
+    }
+
+    var writer = File.Open("NyPrisliste.csv", FileMode.OpenOrCreate, FileAccess.Write);
+    foreach (Vare vare in VareList)
+    {
+        string text = vare.item + ";" + vare.articleDescription + ";" + vare.kostPrisDkk + "Dkk;" + vare.SlagsPris.ToString() + "Dkk;\n";
+        byte[] bytes = System.Text.Encoding.UTF8.GetBytes(text);
+        
+        writer.Write(bytes);
+    }
+    writer.Close();
+    Console.Read();
+}
+
+
+    string itemUpdater(string item)
+    {
+        // Her finder vi bare ordet black og replacer det med sort hvis det er der
+        if (item.Contains("black"))
+        {
+            item = item.Replace("black", "sort");
+        }
+        return item;
+    }
+
+string priceGroupUpdate(string priceGroup)
+{
+    // Her checker jeg hvad fortjenesten er for de forskellige produkter og laver dem og til procent som beskrevet i opgaven
+    string newpriceGroup = priceGroup.ToString() + "%";
+    if (priceGroup == "21")
+    {
+        newpriceGroup = "30%";
+    }
+    else if (priceGroup == "22")
+    {
+        newpriceGroup = "50%";
+    }
+    else if (priceGroup == "23")
+    {
+        newpriceGroup = "40%";
+    }
+    else if (priceGroup == "24")
+    {
+        newpriceGroup = "40%";
+    }
+    else if (priceGroup == "25")
+    {
+        newpriceGroup = "50%";
+    }
+
+    return newpriceGroup;
+}
+
+
+
---
+++ opgave-06-03-2023/Vare.cs
@@ -0,0 +1,66 @@
+﻿using CsvHelper.Configuration.Attributes;
+
+public class Vare
+{
+    // Bruger Bare denne værdi som min kurs håber det er okay
+    // Public bruger jeg så kan senere kan bruge nogle af de neden stående variabler i andre classes og senere Skrive dem i min nye CSV file
+    public static float EuroTODkk = 7.44f;
+    public readonly string item;
+    public readonly string articleDescription = "DataIkkeHentet";
+    private readonly string unit;
+    private readonly float kostPrisEuro;
+    public float kostPrisDkk;
+    private readonly float priceUnit;
+    private readonly string priceGroupe;
+    private readonly DateTime dateOfIssuance;
+    public readonly float SlagsPris;
+
+    public Vare(string itemI, string articleDescriptionI, string unitI, float kostPrisEuroI, float priceUnitI, string priceGroupeI, DateTime dateOfIssuanceI) 
+    { 
+        // Det er sådan her jeg instancere
+        item = itemI;
+        articleDescription = articleDescriptionI;
+        unit = unitI;
+        kostPrisEuro = kostPrisEuroI;
+        priceUnit = priceUnitI;
+        priceGroupe = priceGroupeI;
+        dateOfIssuance = dateOfIssuanceI;
+
+
+        Console.WriteLine("Item Added: "+ item + " " + articleDescription + " " + unit + " " + kostPrisEuro + " " + priceUnit + " " + priceGroupe + " " + dateOfIssuance);
+
+
+        SlagsPris = SalgsPrisCalc(kostPrisEuro, priceGroupe);
+
+
+        Console.WriteLine("salgsPris " + SlagsPris + " Dkk" + " Kostpris " + kostPrisDkk + " Dkk");
+    }
+
+    float SalgsPrisCalc(float euroPris, string priceGroup)
+    {
+
+        kostPrisDkk = euroPris * EuroTODkk;
+
+        //Her udregner jeg salgs prisen ved at lægge for fortjenestes oveni så vi får salgs prisen
+
+        float SalgsPris;
+        if (priceGroup == "30%")
+        {
+            SalgsPris = kostPrisDkk * 1.30f;
+        }
+        else if (priceGroup == "40%")
+        {
+            SalgsPris = kostPrisDkk * 1.40f;
+        }
+        else if (priceGroup == "50%")
+        {
+            SalgsPris = kostPrisDkk * 1.50f;
+        }
+        else
+        {
+            SalgsPris = kostPrisDkk;
+        }
+
+        return SalgsPris;
+    }
+}
